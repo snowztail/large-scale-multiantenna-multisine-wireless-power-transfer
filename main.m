b3b6b470baeb29820b320dc764fa0cc13aa29c80
SnowzTail <i@snowztail.com>
2020-03-06 20:37:36 +0000

    :construction: Single-user WPT

--- main.m
+++ main.m
@@ -1,7 +1,9 @@
 powerBudget = 1;
 nTxs = 4;
 nSubbands = 8;
-beta4 = 1e-3;
+beta2 = 2e-3;
+beta4 = 4e-3;
+threshold = 1e-3;
 
 % \boldsymbol{h}_q
 channel = randn(nTxs * nSubbands, 1);
@@ -9,4 +11,4 @@ channel = randn(nTxs * nSubbands, 1);
 % \boldsymbol{h}_{q,n}
 subchannel = reshape(channel, [nTxs, nSubbands]);
 
-frequencyPrecoder = wpt_su(beta4, powerBudget, nSubbands, nTxs, subchannel);
+frequencyPrecoder = wpt_su(beta2, beta4, powerBudget, nSubbands, nTxs, subchannel, threshold);
--- wpt_su.m
+++ wpt_su.m
@@ -1,19 +1,12 @@
-function frequencyPrecoder = wpt_su(beta4, powerBudget, nSubbands, nTxs, subchannel)
+function [weight] = wpt_su(beta2, beta4, powerBudget, nSubbands, nTxs, subchannel, threshold)
 
-    % \tilde{s}
-    spatialPrecoder = zeros(nTxs * nSubbands, 1);
-    % \tilde{s}_n
-    spatialSubprecoder = reshape(spatialPrecoder, [nTxs, nSubbands]);
 
+
+    %% Initialization
     % ? initialize p by uniform power allocation
-    frequencyPrecoder = sqrt(powerBudget / nSubbands) * ones(nSubbands, 1);
-    for iSubband = 1 : nSubbands
-        spatialSubprecoder(:, iSubband) = conj(subchannel(:, iSubband)) / norm(subchannel(:, iSubband));
-    end
-
+    frequencyWeight = sqrt(powerBudget / nSubbands) * ones(nSubbands, 1);
     % \boldsymbol{X}
-    frequencyPrecoderMatrix = frequencyPrecoder * frequencyPrecoder';
-
+    frequencyWeightMatrix = frequencyWeight * frequencyWeight';
     % \boldsymbol{M}''
     channelNormMatrix = vecnorm(subchannel).' * vecnorm(subchannel);
     % \boldsymbol{M}''_k
@@ -22,10 +15,52 @@ function frequencyPrecoder = wpt_su(beta4, powerBudget, nSubbands, nTxs, subchan
     auxiliary = zeros(nSubbands, 1);
     for iSubband = 1 : nSubbands
         channelNormDiagMatrix{iSubband} = diag(diag(channelNormMatrix, iSubband - 1), iSubband - 1);
-        auxiliary(iSubband) = trace(channelNormDiagMatrix{iSubband} * frequencyPrecoderMatrix);
+        auxiliary(iSubband) = trace(channelNormDiagMatrix{iSubband} * frequencyWeightMatrix);
     end
-    % \boldsymbol{A}_0
-    coefficient = diag(beta4 * [-1.5; -3 * ones(nSubbands - 1, 1)]);
-    % g_{boldsymbol{t}}
-    target4 = auxiliary' * coefficient * auxiliary;
+    %% Iteration
+    isConverged = false;
+    while ~isConverged
+        % \boldsymbol{C}''_1
+        termC1 = - (beta2 + 3 * beta4 * auxiliary(1)) / 2 * channelNormDiagMatrix{1} - 3 * beta4 * sum(cat(3, channelNormDiagMatrix{2 : end}) .* reshape(conj(auxiliary(2 : end)), [1, 1, nSubbands - 1]), 3);
+        % \boldsymbol{A}''_1
+        termA1 = termC1 + termC1';
+
+        % * Solve rank-1 \boldsymbol{X}^{\star} in closed form (low complexity)
+        % \boldsymbol{x}^{\star}
+        [v, d] = eig(termA1);
+        frequencyWeight = sqrt(powerBudget) * v(:, diag(d) == min(diag(d)));
+        clearvars v d;
+        % \boldsymbol{X}^{\star}
+        frequencyPrecoderMatrix_ = frequencyWeight * frequencyWeight';
+        % Update \boldsymbol{t}
+        auxiliary = zeros(nSubbands, 1);
+        for iSubband = 1 : nSubbands
+            auxiliary(iSubband) = trace(channelNormDiagMatrix{iSubband} * frequencyPrecoderMatrix_);
+        end
+        % test convergence
+        if (norm(frequencyPrecoderMatrix_, 'fro') - norm(frequencyWeightMatrix, 'fro')) / norm(frequencyPrecoderMatrix_, 'fro') <= threshold
+            isConverged = true;
+        end
+    end
+    spatialPrecoder = conj(subchannel) ./ vecnorm(subchannel);
+    % s_n
+    weight = zeros(nTxs, nSubbands);
+    for iSubband = 1 : nSubbands
+        weight(:, iSubband) = frequencyWeight(iSubband) * spatialPrecoder(:, iSubband);
+    end
+
 end
+
+% % * Solve high rank \boldsymbol{X} in SDP problem by cvx (high complexity)
+% cvx_begin
+%     cvx_solver mosek
+%     variable frequencyWeightMatrix
+%     minimize trace(termA1 * frequencyWeightMatrix)
+%     subject to
+%         trace(frequencyWeightMatrix) <= powerBudget;
+% cvx_end
+% % update auxiliary terms
+% auxiliary = zeros(nSubbands, 1);
+% for iSubband = 1 : nSubbands
+%     auxiliary(iSubband) = trace(channelNormDiagMatrix{iSubband} * frequencyWeightMatrix);
+% end
