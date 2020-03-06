bcd3ab64f6bb9594aa5081c33763a0f967ce5822
SnowzTail <i@snowztail.com>
2020-03-06 22:11:42 +0000

    :ambulance: Fix convergence test

--- main.m
+++ main.m
@@ -3,12 +3,12 @@ nTxs = 4;
 nSubbands = 8;
 beta2 = 2e-3;
 beta4 = 4e-3;
-threshold = 1e-3;
+tolerance = 1e-3;
 
 % \boldsymbol{h}_q
-channel = randn(nTxs * nSubbands, 1);
+channel = sqrt(1 / 2) * (randn(nTxs * nSubbands, 1) + 1i * randn(nTxs * nSubbands, 1));
 
 % \boldsymbol{h}_{q,n}
 subchannel = reshape(channel, [nTxs, nSubbands]);
 
-frequencyPrecoder = wpt_su(beta2, beta4, powerBudget, nSubbands, nTxs, subchannel, threshold);
+frequencyPrecoder = wpt_su(beta2, beta4, powerBudget, nSubbands, nTxs, subchannel, tolerance);
--- wpt_su.m
+++ wpt_su.m
@@ -1,27 +1,47 @@
-function [weight] = wpt_su(beta2, beta4, powerBudget, nSubbands, nTxs, subchannel, threshold)
+function [waveform] = wpt_su(beta2, beta4, powerBudget, nSubbands, nTxs, subchannel, tolerance)
+% Function:
+%   - optimize the amplitude and phase of transmit multisine waveform
+%
+% InputArg(s):
+%   - beta2 [\beta_2]: diode second-order parameter
+%   - beta4 [\beta_4]: diode fourth-order parameter
+%   - powerBudget [P]: transmit power constraint
+%   - nSubbands [N]: number of subbands/subcarriers
+%   - nTxs [M]: number of transmit antennas
+%   - subchannel [h_{q, n}] (nTxs * nSubbands): channel frequency response matrix
+%   - tolerance [\epsilon]: convergence ratio
+%
+% OutputArg(s):
+%   - waveform: complex waveform weights for each transmit antenna and subband
+%
+% Comment(s):
+%   - the optimal spatial beamforming for SU WPT is MRT
+%   - obtain the rank-1 frequency weight matrix in closed form for a complexity of \mathcal{O}(N ^ 3)
+%   - as all eigenvalues of \boldsymbol{A}''_1 are negative, we choose the eigenvector corresponding to the minimum eigenvalue (maximum in magnitude)
+%
+% Reference(s):
+%   - Y. Huang and B. Clerckx, "Large-Scale Multiantenna Multisine Wireless Power Transfer," IEEE Transactions on Signal Processing, vol. 65, no. 21, pp. 5812–5827, Jan. 2017.
+%
+% Author & Date: Yang (i@snowztail.com) - 06 Mar 20
 
 
-
-    %% Initialization
     % ? initialize p by uniform power allocation
     frequencyWeight = sqrt(powerBudget / nSubbands) * ones(nSubbands, 1);
     % \boldsymbol{X}
     frequencyWeightMatrix = frequencyWeight * frequencyWeight';
-    % \boldsymbol{M}''
-    channelNormMatrix = vecnorm(subchannel).' * vecnorm(subchannel);
     % \boldsymbol{M}''_k
-    channelNormDiagMatrix = cell(nSubbands, 1);
+    channelNormMatrix = cell(nSubbands, 1);
     % \boldsymbol{t}
     auxiliary = zeros(nSubbands, 1);
     for iSubband = 1 : nSubbands
-        channelNormDiagMatrix{iSubband} = diag(diag(channelNormMatrix, iSubband - 1), iSubband - 1);
-        auxiliary(iSubband) = trace(channelNormDiagMatrix{iSubband} * frequencyWeightMatrix);
+        channelNormMatrix{iSubband} = diag(diag(vecnorm(subchannel).' * vecnorm(subchannel), iSubband - 1), iSubband - 1);
+        auxiliary(iSubband) = trace(channelNormMatrix{iSubband} * frequencyWeightMatrix);
     end
-    %% Iteration
+
     isConverged = false;
     while ~isConverged
         % \boldsymbol{C}''_1
-        termC1 = - (beta2 + 3 * beta4 * auxiliary(1)) / 2 * channelNormDiagMatrix{1} - 3 * beta4 * sum(cat(3, channelNormDiagMatrix{2 : end}) .* reshape(conj(auxiliary(2 : end)), [1, 1, nSubbands - 1]), 3);
+        termC1 = - (beta2 + 3 * beta4 * auxiliary(1)) / 2 * channelNormMatrix{1} - 3 * beta4 * sum(cat(3, channelNormMatrix{2 : end}) .* reshape(conj(auxiliary(2 : end)), [1, 1, nSubbands - 1]), 3);
         % \boldsymbol{A}''_1
         termA1 = termC1 + termC1';
 
@@ -31,36 +51,32 @@ function [weight] = wpt_su(beta2, beta4, powerBudget, nSubbands, nTxs, subchanne
         frequencyWeight = sqrt(powerBudget) * v(:, diag(d) == min(diag(d)));
         clearvars v d;
         % \boldsymbol{X}^{\star}
-        frequencyPrecoderMatrix_ = frequencyWeight * frequencyWeight';
+        frequencyWeightMatrix_ = frequencyWeight * frequencyWeight';
+
+        % % * Solve high rank \boldsymbol{X} in SDP problem by cvx (high complexity)
+        % cvx_begin
+        %     cvx_solver mosek
+        %     variable frequencyWeightMatrix_
+        %     minimize trace(termA1 * frequencyWeightMatrix_)
+        %     subject to
+        %         trace(frequencyWeightMatrix_) <= powerBudget;
+        % cvx_end
+
         % Update \boldsymbol{t}
-        auxiliary = zeros(nSubbands, 1);
         for iSubband = 1 : nSubbands
-            auxiliary(iSubband) = trace(channelNormDiagMatrix{iSubband} * frequencyPrecoderMatrix_);
+            auxiliary(iSubband) = trace(channelNormMatrix{iSubband} * frequencyWeightMatrix_);
         end
         % test convergence
-        if (norm(frequencyPrecoderMatrix_, 'fro') - norm(frequencyWeightMatrix, 'fro')) / norm(frequencyPrecoderMatrix_, 'fro') <= threshold
+        if (norm(frequencyWeightMatrix_ - frequencyWeightMatrix, 'fro')) / norm(frequencyWeightMatrix_, 'fro') <= tolerance
             isConverged = true;
         end
+        frequencyWeightMatrix = frequencyWeightMatrix_;
     end
     spatialPrecoder = conj(subchannel) ./ vecnorm(subchannel);
     % s_n
-    weight = zeros(nTxs, nSubbands);
+    waveform = zeros(nTxs, nSubbands);
     for iSubband = 1 : nSubbands
-        weight(:, iSubband) = frequencyWeight(iSubband) * spatialPrecoder(:, iSubband);
+        waveform(:, iSubband) = frequencyWeight(iSubband) * spatialPrecoder(:, iSubband);
     end
 
 end
-
-% % * Solve high rank \boldsymbol{X} in SDP problem by cvx (high complexity)
-% cvx_begin
-%     cvx_solver mosek
-%     variable frequencyWeightMatrix
-%     minimize trace(termA1 * frequencyWeightMatrix)
-%     subject to
-%         trace(frequencyWeightMatrix) <= powerBudget;
-% cvx_end
-% % update auxiliary terms
-% auxiliary = zeros(nSubbands, 1);
-% for iSubband = 1 : nSubbands
-%     auxiliary(iSubband) = trace(channelNormDiagMatrix{iSubband} * frequencyWeightMatrix);
-% end
