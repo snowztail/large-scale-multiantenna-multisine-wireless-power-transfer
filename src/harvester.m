bfd66721fee48a51ba9ad27053d04071aef9c94f
SnowzTail <i@snowztail.com>
2020-03-08 23:07:42 +0000

    :sparkles: Harvester output DC current

--- src/harvester.m
+++ src/harvester.m
@@ -1,6 +1,26 @@
 function voltage = harvester(beta2, beta4, waveform, subchannel)
+    % Function:
+    %   - calculate the harvester output voltage
+    %
+    % InputArg(s):
+    %   - beta2 [\beta_2]: diode second-order parameter
+    %   - beta4 [\beta_4]: diode fourth-order parameter
+    %   - waveform [\boldsymbol{s_n}] (nTxs * nSubbands): complex waveform weights for each transmit antenna and subband
+    %   - subchannel [\boldsymbol{h_{q, n}}] (nRxs * nTxs * nSubbands): channel frequency response at each subband
+    %
+    % OutputArg(s):
+    %   - voltage [v_{\text{out}}]: rectifier output DC voltage
+    %
+    % Comment(s):
+    %   - truncate the voltage expression to the fourth order to capture fundamental behavior of rectifier nonlinearity
+    %
+    % Reference(s):
+    %   - Y. Huang and B. Clerckx, "Large-Scale Multiantenna Multisine Wireless Power Transfer," IEEE Transactions on Signal Processing, vol. 65, no. 21, pp. 5812–5827, Jan. 2017.
+    %
+    % Author & Date: Yang (i@snowztail.com) - 08 Mar 20
 
 
+    % single receive antenna
     [~, ~, nSubbands] = size(subchannel);
     subchannel = squeeze(subchannel);
 
@@ -24,6 +44,6 @@ function voltage = harvester(beta2, beta4, waveform, subchannel)
             end
         end
     end
-    % truncate the voltage expression to the fourth order to capture fundamental behavior of rectifier nonlinearity
     voltage = real(beta2 * term2 + beta4 * term4);
+
 end
