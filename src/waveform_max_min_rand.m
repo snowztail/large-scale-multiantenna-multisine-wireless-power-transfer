aa6f8633add44534b952f18274365f0d2f80bcfc
SnowzTail <i@snowztail.com>
2020-03-17 21:45:54 +0000

    :construction: Max-min: rank reduction and randomization

--- src/waveform_max_min_rand.m
+++ src/waveform_max_min_rand.m
@@ -1,4 +1,4 @@
-function [waveform, sumVoltage, userVoltage, minVoltage] = waveform_max_min_rr(beta2, beta4, powerBudget, channel, tolerance, weight)
+function [waveform, sumVoltage, userVoltage, minVoltage] = waveform_max_min_rand(beta2, beta4, powerBudget, channel, tolerance, weight, nCandidates)
     % Function:
     %   - optimize the amplitude and phase of transmit multisine waveform
     %
@@ -9,6 +9,7 @@ function [waveform, sumVoltage, userVoltage, minVoltage] = waveform_max_min_rr(b
     %   - channel [\boldsymbol{h_{q, n}}] (nTxs * nSubbands * nUsers): channel frequency response at each subband
     %   - tolerance [\epsilon]: convergence ratio
     %   - weight [w_q] (1 * nUsers): user weights
+    %   - nCandidates: number of random feasible rank-1 solutions to generate
     %
     % OutputArg(s):
     %   - waveform [\boldsymbol{s}_n] (nTxs * nSubbands): complex waveform weights for each transmit antenna and subband
@@ -18,12 +19,11 @@ function [waveform, sumVoltage, userVoltage, minVoltage] = waveform_max_min_rr(b
     %
     % Comment(s):
     %   - maximize the minimum user voltage
-    %   - for multi-user MISO systems with number of user no larger than 3
-    %   - in each iteration, we first obtain the high rank covariance matrix by CVX, then perform rank reduction for a rank-1 solution
+    %   - for multi-user MISO systems with arbitrary number of user
+    %   - we first obtain the high rank covariance matrix by CVX, then generate waveform candidates based on random vectors and pick the optimal one as a rank-1 solution
     %
     % Reference(s):
     %   - Y. Huang and B. Clerckx, "Large-Scale Multiantenna Multisine Wireless Power Transfer," IEEE Transactions on Signal Processing, vol. 65, no. 21, pp. 5812–5827, Jan. 2017.
-    %   - Y. Huang and D. Palomar, "Rank-Constrained Separable Semidefinite Programming With Applications to Optimal Beamforming," IEEE Transactions on Signal Processing, vol. 58, no. 2, pp. 664–678, 2010.
     %
     % Author & Date: Yang (i@snowztail.com) - 17 Mar 20
 
@@ -83,32 +83,7 @@ function [waveform, sumVoltage, userVoltage, minVoltage] = waveform_max_min_rr(b
             subject to
                 trace(highRankWaveformMatrix) <= powerBudget;
         cvx_end
-        [~, userIndex] = max(target);
-
-        % * Rank reduction for separable SDP
-        waveformRank = rank(highRankWaveformMatrix);
-        % matrix to reduce rank
         waveformMatrix_ = highRankWaveformMatrix;
-        while waveformRank ^ 2 > nUsers
-            % decompose waveform matrix as a product of a matrix V and its Hermitian
-            waveformFactor = cholcov(waveformMatrix_)';
-            % flatten trace equations to standard linear equations
-            coefficient = zeros(nUsers, size(waveformFactor, 2) ^ 2);
-            for iUser = 1 : nUsers
-                coefficient(iUser, :) = reshape((waveformFactor' * (termA1{iUser} - termA1{userIndex}) * waveformFactor).', 1, []);
-            end
-            % obtain an orthonormal basis for the null space of the coefficient matrix
-            delta = null(coefficient);
-            % nonzero solution can be obtained as a linear combination of null space basis vectors
-            delta = reshape(delta(:, 1), [waveformRank, waveformRank]);
-            delta = (delta + delta') / 2;
-            [~, d] = eig(delta);
-            dominantEigenvalue = d(abs(d) == max(abs(d)));
-            % there can be multiple equivalent entries with largest magnitude and we only use the first one
-            waveformMatrix_ = waveformFactor * (eye(waveformRank) - 1 / dominantEigenvalue(1) * delta) * waveformFactor';
-            % update waveform rank
-            waveformRank = rank(waveformMatrix_);
-        end
 
         % Update \boldsymbol{t}_{q, k}
         for iUser = 1 : nUsers
@@ -122,11 +97,49 @@ function [waveform, sumVoltage, userVoltage, minVoltage] = waveform_max_min_rr(b
             isConverged = true;
         end
         waveformMatrix = waveformMatrix_;
-
     end
 
-    % decompose waveform matrix for the waveform vector
-    waveform = cholcov(waveformMatrix)';
+    % * Derive a best rank-1 solution from randomly generated vectors
+    [v, d] = eig(waveformMatrix);
+    % generate waveform candidates based on random vectors
+    candidateWaveform = cell(nCandidates, 1);
+    candidateTarget = zeros(nCandidates, nUsers);
+    for iCandidate = 1 : nCandidates
+        radius = rand(nTxs * nSubbands, 1);
+        phase = 2 * pi * rand(nTxs * nSubbands, 1);
+        candidateWaveform{iCandidate} = v * d ^ (1 / 2) * (sqrt(radius) .* (cos(phase) + 1i * sin(phase)));
+        candidateWaveformMatrix = candidateWaveform{iCandidate} * candidateWaveform{iCandidate}';
+
+        % Update \boldsymbol{t}_{q, k}
+        for iUser = 1 : nUsers
+            for iSubband = 1 : nSubbands
+                auxiliary(iUser, iSubband) = trace(channelMatrix{iUser, iSubband} * candidateWaveformMatrix);
+            end
+        end
+
+        % \bar{c}_q
+        termBarC = zeros(1, nUsers);
+        % \boldsymbol{C}_{q, 1}
+        termC1 = cell(1, nUsers);
+        % \boldsymbol{A}_{q, 1}
+        termA1 = cell(1, nUsers);
+        for iUser = 1 : nUsers
+            termBarC(iUser) = - real(conj(auxiliary(iUser, :)) * termA0 * auxiliary(iUser, :).');
+            termC1{iUser} = - ((beta2 + 3 * beta4 * auxiliary(iUser, 1)) / 2 * channelMatrix{iUser, 1});
+            if nSubbands > 1
+                termC1{iUser} = termC1{iUser} - weight(iUser) * 3 * beta4 * sum(cat(3, channelMatrix{iUser, 2 : end}) .* reshape(conj(auxiliary(iUser,2 : end)), [1, 1, nSubbands - 1]), 3);
+            end
+            termA1{iUser} = termC1{iUser} + termC1{iUser}';
+        end
+
+        for iUser = 1 : nUsers
+            candidateTarget(iCandidate, iUser) = real(trace(termA1{iUser} * candidateWaveformMatrix) + termBarC(iUser));
+        end
+
+    end
+    [~, candidateIndex] = min(max(candidateTarget, [], 2));
+
+    waveform = candidateWaveform{candidateIndex};
 
     % v_{\text{out}, q}
     userVoltage = zeros(1, nUsers);
