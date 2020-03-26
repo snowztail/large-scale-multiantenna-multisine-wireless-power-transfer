493f1c12a562966787d4ef91f963bc1c527376cd
SnowzTail <i@snowztail.com>
2020-03-26 04:59:23 +0000

    :wrench: Update config

--- config_mu_comparison.m
+++ config_mu_comparison.m
@@ -13,10 +13,10 @@ carrierFrequency = centerFrequency - bandwidth * (1 - 1 / nSubbands) / 2: bandwi
 distance = 10;
 pathlossExponent = 2;
 pathloss = db2pow(60.046 + 10 * pathlossExponent * log10(distance / 10)) * ones(1, nUsers);
-nRealizations = 1e2;
+nRealizations = 5;
 fadingType = 'selective';
 %% Harvester
 % assumptions: antenna impedance = 50 ohms, ideality factor = 1, thermal voltage = 25.85 mV
 beta2 = 9.6712e2;
 beta4 = 6.0304e6;
-tolerance = 1e-3;
+tolerance = 5e-3;
--- wpt_mu_comparison.m
+++ wpt_mu_comparison.m
@@ -1,4 +1,9 @@
 clear; close all; clc; initialize; config_mu_comparison;
 %% Waveform design by Max-Min-Rand, CHE Max-Min-Rand, and CHE Max-Min-RR algorithms
 channel = channel_tgn_e(pathloss, nTxs, nSubbands, nUsers, carrierFrequency, fadingType);
+% [~, ~, ~, minVoltageRr] = waveform_max_min_rr(beta2, beta4, powerBudget, channel, tolerance, weight);
 [~, ~, ~, minVoltageCheRr] = waveform_max_min_che_rr(beta2, beta4, powerBudget, channel, tolerance, weight, pathloss);
+minVoltageCheRand = zeros(nRealizations, 1);
+for iRealization = 1 : nRealizations
+    [~, ~, ~, minVoltageCheRand(iRealization)] = waveform_max_min_che_rand(beta2, beta4, powerBudget, channel, tolerance, weight, pathloss);
+end
