d4e65b937817e909e6e6425c6aa6a32ecbae6a2d
SnowzTail <i@snowztail.com>
2020-03-29 22:27:19 +0100

    :sparkles: Max-min comparison

--- config_max_min_comparison.m
+++ config_max_min_comparison.m
@@ -1,22 +1,19 @@
-%% User
-nUsers = 2;
-weight = ones(1, nUsers);
 %% Transceiver
 eirp = db2pow(36 - 30);
-nTxs = 4;
-powerBudget = eirp / nTxs;
+nCandidates = 50;
 %% Channel
 centerFrequency = 2.4e9;
 bandwidth = 1e7;
-nSubbands = 8;
-carrierFrequency = centerFrequency - bandwidth * (1 - 1 / nSubbands) / 2: bandwidth / nSubbands: centerFrequency + bandwidth * (1 - 1 / nSubbands) / 2;
 distance = 10;
 pathlossExponent = 2;
-pathloss = db2pow(60.046 + 10 * pathlossExponent * log10(distance / 10)) * ones(1, nUsers);
 nRealizations = 1;
 fadingType = 'selective';
 %% Harvester
 % assumptions: antenna impedance = 50 ohms, ideality factor = 1, thermal voltage = 25.85 mV
 beta2 = 9.6712e2;
 beta4 = 6.0304e6;
-tolerance = 1e-6;
+tolerance = 1e-3;
+%% Variables
+Variable.nTxs = [4, 20];
+Variable.nSubbands = [4, 8];
+Variable.nUsers = 2 : 5;
--- wpt_max_min_comparison.m
+++ wpt_max_min_comparison.m
@@ -1,3 +1,37 @@
 clear; close all; clc; initialize; config_max_min_comparison;
-channel = channel_tgn_e(pathloss, nTxs, nSubbands, nUsers, carrierFrequency, fadingType);
-[~, ~, ~, minVoltage] = waveform_max_min_che_rr(beta2, beta4, powerBudget, channel, tolerance, weight, pathloss);
+%% Waveform design by Max-Min-Rand, CHE Max-Min-RR and CHE Max-Min-Rand algorithms
+minVoltageRand = zeros(length(Variable.nTxs), length(Variable.nSubbands), length(Variable.nUsers), nRealizations);
+minVoltageCheRr = zeros(length(Variable.nTxs), length(Variable.nSubbands), length(Variable.nUsers), nRealizations);
+minVoltageCheRand = zeros(length(Variable.nTxs), length(Variable.nSubbands), length(Variable.nUsers), nRealizations);
+for iTx = 1 : length(Variable.nTxs)
+    nTxs = Variable.nTxs(iTx);
+    powerBudget = eirp / nTxs;
+    for iSubband = 1 : length(Variable.nSubbands)
+        nSubbands = Variable.nSubbands(iSubband);
+        carrierFrequency = centerFrequency - bandwidth * (1 - 1 / nSubbands) / 2: bandwidth / nSubbands: centerFrequency + bandwidth * (1 - 1 / nSubbands) / 2;
+        for iUser = 1 : length(Variable.nUsers)
+            nUsers = Variable.nUsers(iUser);
+            weight = ones(1, nUsers);
+            pathloss = db2pow(60.046 + 10 * pathlossExponent * log10(distance / 10)) * ones(1, nUsers);
+            for iRealization = 1 : nRealizations
+                channel = channel_tgn_e(pathloss, nTxs, nSubbands, nUsers, carrierFrequency, fadingType);
+                [~, ~, ~, minVoltageRand(iTx, iSubband, iUser, iRealization)] = waveform_max_min_rand(beta2, beta4, powerBudget, channel, tolerance, weight, nCandidates);
+                [~, ~, ~, minVoltageCheRr(iTx, iSubband, iUser, iRealization)] = waveform_max_min_che_rr(beta2, beta4, powerBudget, channel, tolerance, weight, pathloss);
+                [~, ~, ~, minVoltageCheRand(iTx, iSubband, iUser, iRealization)] = waveform_max_min_che_rand(beta2, beta4, powerBudget, channel, tolerance, weight, pathloss);
+            end
+        end
+    end
+end
+minVoltageRand = mean(minVoltageRand, 4);
+minVoltageCheRr = mean(minVoltageCheRr, 4);
+minVoltageCheRand = mean(minVoltageCheRand, 4);
+save('data/wpt_max_min_comparison.mat');
+%% Result
+figure('Name', sprintf('Average minimum output voltage as a function of (M, N, K)'));
+bar(1e3 * [vec(minVoltageRand), vec(minVoltageCheRr), vec(minVoltageCheRand)]);
+label = [repelem(Variable.nTxs, length(Variable.nSubbands) * length(Variable.nUsers)); repmat(repelem(Variable.nSubbands, length(Variable.nUsers)), [1, length(Variable.nTxs)]); repmat(repmat(Variable.nUsers, [1, length(Variable.nSubbands)]), [1, length(Variable.nTxs)])];
+set(gca, 'XTick', 1 : length(Variable.nTxs) * length(Variable.nSubbands) * length(Variable.nUsers), 'xticklabel', display_coordinate(label));
+grid on;
+legend('Max-Min-Rand', 'CHE Max-Min-RR', 'CHE Max-Min-Rand', 'location', 'nw');
+ylabel('Average v_{out} [mV]')
+savefig('results/wpt_max_min_comparison.fig');
